296e614db5efd19cea0efc7822aafeaab3385b2e
Emirhan Yaman <90368997+yamanemirhan@users.noreply.github.com>
2023-09-27 19:43:34 +0300

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -91,6 +91,8 @@ const editDetails = asyncErrorWrapper(async (req, res, next) => {
     const user_ = await User.findById(req.user.id);
     const userComments = await Comment.find({ "user.id": req.user.id });
     const fullName = user_.firstName[0] + "**** " + user_.lastName[0] + "****";
+    console.log(user_);
+    console.log("comments: ", userComments);
     for (const comment of userComments) {
       comment.user.profilePicture = user_.profilePicture;
       comment.user.fullName = fullName;
